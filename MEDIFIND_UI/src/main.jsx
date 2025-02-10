bb05ef412faed0f2283c3fe91620ce7d6f294b16
Aya27ashour <yoyo.ashour27@gmail.com>
2025-02-10 22:58:18 +0200

    creat registration page

--- MEDIFIND_UI/src/main.jsx
+++ MEDIFIND_UI/src/main.jsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { MainLayout } from './layout/MainLayout'
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 createRoot(document.getElementById('root')).render(
   <MainLayout />
--- MEDIFIND_UI/src/pages/registeration.jsx
+++ MEDIFIND_UI/src/pages/registeration.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Container, Card, Form, Button } from "react-bootstrap";
 
-export default function registeration() {
+
+export  function Registeration() {
   return (
-    <div>
-      
-    </div>
+    <>
+        <Container className="d-flex justify-content-center align-items-center vh-100" >
+
+      <Card style={{ width: "400px", borderRadius: "10px" }} className="p-4">
+        <h3 className="text-center mb-4">Registration</h3>
+
+        <Form>
+           <Form.Group className="mb-3" controlId="formName">
+            <Form.Label>Enter your name</Form.Label>
+            <Form.Control type="text" placeholder="Enter your name" className="form-control"/></Form.Group>
+
+          <Form.Group className="mb-3" controlId="formEmail">
+            <Form.Label>Enter your email</Form.Label>
+            <Form.Control type="email"placeholder="Enter your email" className="form-control" /></Form.Group>
+
+          <Form.Group className="mb-3" controlId="formPassword">
+            <Form.Label>Enter your Password</Form.Label>
+            <Form.Control type="password" placeholder="Enter your Password" className="form-control" /></Form.Group>
+
+          <Form.Group className="mb-3" controlId="formConfirmPassword">
+            <Form.Label>Confirm your password</Form.Label>
+            <Form.Control type="password" placeholder="Confirm your password" className="form-control" /> </Form.Group>
+
+          <div className="text-center">
+            <Button type="submit"className="w-100" style={{backgroundColor: "#00796b",border: "none",borderRadius: "5px", }}>
+              Submit
+            </Button>
+          </div>
+        </Form>
+      </Card>
+    </Container>
+    </>
   )
 }
