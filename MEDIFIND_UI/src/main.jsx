534ff94c1d9abc294b00d44a03962a57571220f0
ziadakram18 <ziadakram313@gmail.com>
2025-02-10 20:45:54 +0200

    creat elproject ya gd3an

---
+++ MEDIFIND_UI/src/main.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { MainLayout } from './layout/MainLayout'
+
+
+createRoot(document.getElementById('root')).render(
+  <MainLayout />
+)
