57dbb0a034524dab23d172e0ba3ce3548b100a9c
Aya27ashour <yoyo.ashour27@gmail.com>
2025-02-11 09:31:55 +0200

    change style in registration page

--- src/pages/registeration.jsx
+++ src/pages/registeration.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Card, Form, Button } from "react-bootstrap";
+import { AddBtn } from "../Custom/Addbtn";
 
 
 export  function Registeration() {
@@ -7,31 +8,32 @@ export  function Registeration() {
     <>
         <Container className="d-flex justify-content-center align-items-center vh-100" >
 
-      <Card style={{ width: "400px", borderRadius: "10px" }} className="p-4">
-        <h3 className="text-center mb-4">Registration</h3>
+      <Card style={{ backgroundColor: "#d6d2d2", maxWidth: "100%", width: "400px", borderRadius: "10px" }} className="p-4 w-100 w-md-75 w-lg-50 w-xs-25">
+        <h3 className="text-center mb-4 fw-bold">Registration</h3>
 
         <Form>
            <Form.Group className="mb-3" controlId="formName">
-            <Form.Label>Enter your name</Form.Label>
+            <Form.Label>User Name</Form.Label>
             <Form.Control type="text" placeholder="Enter your name" className="form-control"/></Form.Group>
 
           <Form.Group className="mb-3" controlId="formEmail">
-            <Form.Label>Enter your email</Form.Label>
+            <Form.Label>Email</Form.Label>
             <Form.Control type="email"placeholder="Enter your email" className="form-control" /></Form.Group>
 
           <Form.Group className="mb-3" controlId="formPassword">
-            <Form.Label>Enter your Password</Form.Label>
+            <Form.Label>Password</Form.Label>
             <Form.Control type="password" placeholder="Enter your Password" className="form-control" /></Form.Group>
 
           <Form.Group className="mb-3" controlId="formConfirmPassword">
-            <Form.Label>Confirm your password</Form.Label>
+            <Form.Label>Confirm password</Form.Label>
             <Form.Control type="password" placeholder="Confirm your password" className="form-control" /> </Form.Group>
-
-          <div className="text-center">
-            <Button type="submit"className="w-100" style={{backgroundColor: "#00796b",border: "none",borderRadius: "5px", }}>
-              Submit
-            </Button>
+            <br/>
+          <div className="text-center d-flex justify-content-end">
+            <AddBtn type="submit">
+              Sign Up
+            </AddBtn>
           </div>
+          <br/>
         </Form>
       </Card>
     </Container>
