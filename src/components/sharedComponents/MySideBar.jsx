a5a12bbaadd9f515d3b5e4dd8c2014b8fe380ab5
HebaElgohary <hebasamir535@gmial.com>
2025-02-15 18:00:53 +0200

    linked offerReviews with getAllMedicine end point

--- src/components/sharedComponents/MySideBar.jsx
+++ src/components/sharedComponents/MySideBar.jsx
@@ -4,11 +4,11 @@ import { AddBtn } from "../customComponents/Addbtn";
 export const MySideBar = () => {
   return (
   <div className="sidebar d-flex flex-column p-5">
-    <a href="" style={{textDecoration:'none',paddingBottom:"10px"}}>Needs  </a>
-    <a href="" style={{textDecoration:'none',paddingBottom:"10px"}}>Settings</a>
-    <a href="" style={{textDecoration:'none',paddingBottom:"10px"}}>Help Center </a>
-    <a href="" style={{textDecoration:'none',paddingBottom:"10px"}}>Contact us </a>
-     <AddBtn style={{'width':'45%','margin-top':30,'margin-left':0,'background-color':'#1E9694' }} >Logout</AddBtn>
+    <a href="" style={{"textDecoration":'none','paddingBottom':"10px"}}>Needs  </a>
+    <a href="" style={{"textDecoration":'none','paddingBottom':"10px"}}>Settings</a>
+    <a href="" style={{"textDecoration":'none','paddingBottom':"10px"}}>Help Center </a>
+    <a href="" style={{"textDecoration":'none','paddingBottom':"10px"}}>Contact us </a>
+     <AddBtn style={{'width':'45%','marginTop':30,'marginLeft':0,'backgroundColor':'#1E9694' }} >Logout</AddBtn>
   </div>
        
 
