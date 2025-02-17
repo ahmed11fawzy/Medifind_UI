853ae75d91e68b0075d05b033ee214538bcdb3ac
Aya27ashour <yoyo.ashour27@gmail.com>
2025-02-17 14:34:55 +0200

    add styled btn

---
+++ src/components/customComponents/Styledbtn.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+export const StyledButton = styled(motion.button)`
+   padding: 12px 20px;
+  font-family: "sofia-pro", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  font-size: 16px;
+  color: #fff;
+  border: none;
+  background-color:#01B3BD;
+  cursor: pointer;
+  border-radius: 8px;
+  width: 160px;
+  transition: transform 0.2s;
+  width: 100%;
+
+  &:hover {
+    transform: scale(1.05);
+    background-color: #42ced5;
+  }
+
+  &:active {
+    transform: scale(0.95);
+  }
+`;
+export const Styledbtn = ({ onClick, children }) => {
+  return (
+    <StyledButton onClick={onClick} whileTap={{ scale: 0.95 }}>
+      {children}
+    </StyledButton>
+  );
+};
