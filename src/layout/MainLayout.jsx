cf69fcf91ab05d6e5657ca93fcf57724e0544dcf
reus <af6394158@gmail.com>
2025-02-17 13:46:31 +0200

    updating home page

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,7 +2,7 @@
 import { BrowserRouter , Routes, Route,  } from "react-router-dom";
 import { Login } from '../pages/Login';
 // import { Home } from '../pages/Home';
-import {Home} from "../pages/Home";
+import {Home} from "../pages/Home/Home";
 import {OffersReview} from "../pages/OffersReview"
 import { AddMedicine } from '../pages/AddMedicine';
 import { RequestMedicine } from '../pages/RequestMedicine';
