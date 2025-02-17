145786c52329abe743780f5fe6e27d86523e2a7e
HebaElgohary <hebasamir535@gmial.com>
2025-02-18 01:20:13 +0200

    fixed medicineCard rendering styled AddBtn

--- src/pages/OffersReview.jsx
+++ src/pages/OffersReview.jsx
@@ -14,7 +14,7 @@ export const OffersReview = () => {
 useEffect(() => {
   const fetchMedicines = async () => {
     try {
-      const response = await fetch("http://localhost:7777/getAllMedicine");
+      const response = await fetch("http://localhost:7777/medicine");
       console.log(response);
       if (!response.ok) {
         throw new Error(`Server error: ${response.status} - ${response.statusText}`);
