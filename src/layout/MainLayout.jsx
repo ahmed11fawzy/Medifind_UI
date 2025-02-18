fa4214606cf5e6a68521da189ff1e1ee6833c7e6
reus <af6394158@gmail.com>
2025-02-18 13:17:38 +0200

    updating Add medicine page

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,34 +1,35 @@
-
-import { BrowserRouter , Routes, Route,  } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from '../pages/Login';
-// import { Home } from '../pages/Home';
-import {Home} from "../pages/Home/Home";
-import {OffersReview} from "../pages/OffersReview"
+import { Home } from "../pages/Home/Home";
+import { OffersReview } from "../pages/OffersReview";
 import { AddMedicine } from '../pages/AddMedicine';
 import { RequestMedicine } from '../pages/RequestMedicine';
 import { RequestsReview } from '../pages/RequestsReview';
 import SharedLayout from './SharedLayout';
 import { SignUp } from '../pages/signup/SignUp';
-import { CardPage } from '../pages/CardPage';
+import ProtectedRoute from '../pages/ProtectedRoute'; // Import the ProtectedRoute component
+
 export function MainLayout() {
+  const token = localStorage.getItem('token'); // Check for token in local storage
+  const isAuthenticated = !!token; // Determine if user is authenticated
+
   return (
     <BrowserRouter>
       <Routes>
-
         <Route path="/" element={<SignUp />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
-
-        <Route  element={<SharedLayout />}>
-             <Route path="/home" element={<Home />} />
-             <Route path="/offersReview" element={<OffersReview />} />
-             <Route path="/AddMedicine" element={<AddMedicine />} />
-             <Route path="/RequestMedicine" element={<RequestMedicine />} />
-             <Route path="/RequestsReview" element={<RequestsReview />} />
-           
-
-        </Route>
         
+        <Route element={<SharedLayout />}>
+          {/* Wrap protected routes with ProtectedRoute */}
+          <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}>
+            <Route path="/home" element={<Home />} />
+            <Route path="/offersReview" element={<OffersReview />} />
+            <Route path="/AddMedicine" element={<AddMedicine />} />
+            <Route path="/RequestMedicine" element={<RequestMedicine />} />
+            <Route path="/RequestsReview" element={<RequestsReview />} />
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
