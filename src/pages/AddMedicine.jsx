2704508b826c045d0fc7b4f2b83bd852ee091a82
Aya27ashour <yoyo.ashour27@gmail.com>
2025-02-20 18:54:19 +0200

    add nav style

--- src/pages/AddMedicine.jsx
+++ src/pages/AddMedicine.jsx
@@ -100,7 +100,7 @@ export const AddMedicine = () => {
 
   return (
     <>
-      <Container style={{ marginTop: "50px", marginBottom: "50px" }}>
+      <Container style={{ marginTop: "50px", marginBottom: "50px", marginLeft: "100px"}}>
         <Card className="p-4 shadow-sm">
           <h3 className="text-center mb-4">Add Medicine</h3>
           <Form onSubmit={handleSubmit}>
