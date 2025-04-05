f54b2f794591871217f387852841c3a5cdb5647d
ziadakram18 <ziadakram313@gmail.com>
2025-04-05 16:52:01 +0200

    last

--- src/components/customComponents/CardDonation.jsx
+++ src/components/customComponents/CardDonation.jsx
@@ -55,7 +55,7 @@ export const CardDonation = ({ image, name, quantity, pcs, expDate, onRemove,OnU
             </div>}
             <div className="d-flex gap-3">
               <AddBtn
-              onClick={onRemove}
+              onClick={onRemove()}
               style={{
                 backgroundColor: "#1E9694",
                 border: "none",
